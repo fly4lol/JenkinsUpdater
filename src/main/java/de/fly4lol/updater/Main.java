3fab65ca476f08ff19ed5a4d441f2d01141c78e1
fly4lol <f.pohli.1@gmx.de>
2014-11-30 22:59:56 +0100

    adding config methods class

--- src/main/java/de/fly4lol/updater/Main.java
+++ src/main/java/de/fly4lol/updater/Main.java
@@ -1,5 +1,20 @@
 package de.fly4lol.updater;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.fly4lol.updater.config.Config;
+
+public class Main extends JavaPlugin{
+	public Config config;
+	
+	
+	@Override
+	public void onEnable(){
+		this.config = new Config(this);
+	}
+	
+	public Config getUpdaterConfig(){
+		return config;
+	}
 
 }
